000d46bfe6091b58a945d5bc735f69a50d74583a
Noe Raffaelli <37204563+noeraffa@users.noreply.github.com>
2023-07-22 18:13:17 -0300

    Add files via upload

---
+++ dist/js/tiny-slider-init.js
@@ -0,0 +1,140 @@
+
+//Tiny slider
+if(document.getElementsByClassName('tiny-three-item').length > 0) {
+    var slider = tns({
+        container: '.tiny-three-item',
+        controls: true, // Habilitar las flechas
+        controlsContainer: '.carousel-wrapper',
+    prevButton: '.carousel-prev', // Selector del elemento de flecha "Anterior"
+    nextButton: '.carousel-next', // Selector del elemento de flecha "Siguiente"
+    // Resto de opciones de configuración
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: false,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        speed: 400,
+        gutter: 12,
+        nav: false,
+        responsive: {
+            992: {
+                items: 3
+            },
+
+            767: {
+                items: 2
+            },
+
+            320: {
+                items: 1
+            },
+        },
+    });
+};
+
+if(document.getElementsByClassName('tiny-single-item').length > 0) {
+    var slider = tns({
+        container: '.tiny-single-item',
+        items: 1,
+        controls: false,
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        speed: 400,
+        gutter: 16,
+    });
+};
+
+if(document.getElementsByClassName('tiny-two-item').length > 0) {
+    var slider = tns({
+        container: '.tiny-two-item',
+        controls: false,
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        speed: 400,
+        gutter: 12,
+        responsive: {
+            992: {
+                items: 2
+            },
+
+            767: {
+                items: 2
+            },
+
+            320: {
+                items: 1
+            },
+        },
+    });
+};
+
+if(document.getElementsByClassName('tiny-four-item').length > 0) {
+    var slider = tns({
+        container: '.tiny-four-item',
+        controls: false,
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        speed: 400,
+        gutter: 12,
+        responsive: {
+            992: {
+                items: 4
+            },
+
+            767: {
+                items: 2
+            },
+
+            320: {
+                items: 1
+            },
+        },
+    });
+};
+
+if(document.getElementsByClassName('tiny-six-item').length > 0) {
+    var slider = tns({
+        container: '.tiny-six-item',
+        controls: false,
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        speed: 400,
+        gutter: 12,
+        responsive: {
+            992: {
+                items: 6
+            },
+
+            767: {
+                items: 3
+            },
+
+            320: {
+                items: 1
+            },
+        },
+    });
+};
+
